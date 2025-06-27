8e1ae67a44fa1d2003a6f10bbc57b0264f982b47
Sakuna Sanka <sakuna@Sakunas-MacBook-Air.local>
2025-06-27 20:03:35 +0530

    [SONA-17] Fix some lines to host the database in avien

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,12 +6,18 @@ dotenv.config();
 const sequelize = new Sequelize(
   process.env.DB_NAME!,
   process.env.DB_USER!,
-  process.env.DB_PASSWORD,
+  process.env.DB_PASSWORD!,
   {
     host: process.env.DB_HOST,
     port: parseInt(process.env.DB_PORT || '5432'),
     dialect: 'postgres',
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Use this if you're connecting to a cloud database with SSL
+      }
+    }
   }
 );
 
