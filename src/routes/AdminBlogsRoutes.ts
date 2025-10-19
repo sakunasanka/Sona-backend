28624e0b8140acabc179f0d7e5e0540f137da14a
Parami Anupama <paramianupama2001@gmail.com>
2025-10-19 23:30:27 +0530

    [SONA-80] admin blogs backend updated code

--- src/routes/AdminBlogsRoutes.ts
+++ src/routes/AdminBlogsRoutes.ts
@@ -1,10 +1,15 @@
 import express from 'express';
-import { getAllPostsHandler, updatePostStatusHandler } from '../controllers/AdminBlogsController';
+import { 
+  getAllPostsHandler, 
+  updatePostStatusHandler, 
+  revokePostStatusHandler 
+} from '../controllers/AdminBlogsController';
 import { authenticateToken } from '../middlewares/auth';
 
 const router = express.Router();
 
 router.get('/', authenticateToken, getAllPostsHandler);                         // GET all posts
 router.patch('/:id/status', authenticateToken, updatePostStatusHandler);        // PATCH to update status
+router.patch('/:id/revoke', authenticateToken, revokePostStatusHandler);        // PATCH to revoke status
 
 export default router;
